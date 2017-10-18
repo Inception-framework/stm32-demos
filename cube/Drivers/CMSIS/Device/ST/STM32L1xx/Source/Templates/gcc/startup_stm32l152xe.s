7240cb78d0b5f116937327104bf5a0451d6fb735
Giovanni <camurati@eurecom.fr>
2017-10-18 21:44:39 +0200

    .ld and .s names updated for dynamic interrupt_vector in klee

--- cube/Drivers/CMSIS/Device/ST/STM32L1xx/Source/Templates/gcc/startup_stm32l152xe.s
+++ cube/Drivers/CMSIS/Device/ST/STM32L1xx/Source/Templates/gcc/startup_stm32l152xe.s
@@ -134,7 +134,7 @@ Infinite_Loop:
 * 0x0000.0000.
 *
 ******************************************************************************/
-   .section .isr_vector,"a",%progbits
+   .section .interrupt_vector,"a",%progbits
   .type g_pfnVectors, %object
   .size g_pfnVectors, .-g_pfnVectors
 
